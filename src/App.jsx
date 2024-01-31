1fbcadacbaba089114ed3e3078f644e318c8d1f1
Umesh Singadi <umeshsingadi007@gmail.com>
2024-01-31 13:07:40 +0530

    Add PropTypes to Button component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,14 @@
+import Button from "./components/Button";
 function App() {
-  return <div>App</div>;
+  return (
+    <div>
+      <Button primary>Primary</Button>
+      <Button secondary>Secondary</Button>
+      <Button success>Success</Button>
+      <Button warning>Warning</Button>
+      <Button danger>Danger</Button>
+    </div>
+  );
 }
 
 export default App;
