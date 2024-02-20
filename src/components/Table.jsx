264753032c4d8211a7f969fb0d1b15d3d54c40ff
Umesh Singadi <umeshsingadi007@gmail.com>
2024-02-20 21:20:03 +0530

    Add content to table and make it reusable

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,5 +1,31 @@
-function Table() {
-  return <div>Table</div>;
+function Table({ data, config, keyFn }) {
+  const renderedHeaders = config.map((column) => {
+    return <th key={column.label}>{column.label}</th>;
+  });
+
+  const renderedRows = data.map((rowData) => {
+    const renderedCells = config.map((colum) => {
+      return (
+        <td className="p-3" key={colum.label}>
+          {colum.render(rowData)}
+        </td>
+      );
+    });
+    return (
+      <tr className="border-b" key={keyFn(rowData)}>
+        {renderedCells}
+      </tr>
+    );
+  });
+
+  return (
+    <table className="table-auto border-spacing-2">
+      <thead>
+        <tr className="border-b-2">{renderedHeaders}</tr>
+      </thead>
+      <tbody>{renderedRows}</tbody>
+    </table>
+  );
 }
 
 export default Table;
