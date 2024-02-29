33d5991900e01d3bde7dd61c7ccce49f81e1133f
Umesh Singadi <umeshsingadi007@gmail.com>
2024-02-29 17:22:09 +0530

    Create custom hook for SortableTable

--- src/components/SortableTable.jsx
+++ src/components/SortableTable.jsx
@@ -1,29 +1,10 @@
-import { useState } from "react";
 import Table from "./Table";
+import useSort from "../hooks/use-sort";
 import { FaAngleDown, FaAngleUp } from "react-icons/fa6";
 
 function SortableTable(props) {
-  const [sortOrder, setSortOrder] = useState(null);
-  const [sortBy, setSortBy] = useState(null);
   const { config, data } = props;
-
-  const handleClick = (label) => {
-    if (sortBy && label !== sortBy) {
-      setSortOrder("ase");
-      sortBy(label);
-      return;
-    }
-    if (sortOrder === null) {
-      setSortOrder("ase");
-      setSortBy(label);
-    } else if (sortOrder === "ase") {
-      setSortOrder("des");
-      setSortBy(label);
-    } else if (sortOrder === "des") {
-      setSortOrder(null);
-      setSortBy(null);
-    }
-  };
+  const { sortBy, sortOrder, handleClick, sortedData } = useSort(data, config);
   const updatedConfig = config.map((column) => {
     if (!column.sortValue) {
       return column;
@@ -44,22 +25,6 @@ function SortableTable(props) {
     };
   });
 
-  let sortedData = data;
-
-  if (sortBy && sortOrder) {
-    const { sortValue } = config.find((column) => column.label === sortBy);
-    sortedData = [...data].sort((a, b) => {
-      const valueA = sortValue(a);
-      const valueB = sortValue(b);
-      const reverseOrder = sortOrder === "ase" ? 1 : -1;
-
-      if (typeof valueA === "string") {
-        return valueA.localeCompare(valueB) * reverseOrder;
-      } else {
-        return (valueA - valueB) * reverseOrder;
-      }
-    });
-  }
   return (
     <div>
       <Table {...props} config={updatedConfig} data={sortedData}></Table>
